7625e7c225e5b6af1353833ed2989a07b2058d88
Fatimah Ulwiyatul Badriyah <faatimahbadriyah@gmail.com>
2021-10-05 22:22:27 +0700

    update connection, sql

--- cek_login.php
+++ cek_login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // mengaktifkan session pada php
 session_start();
 
@@ -9,32 +9,29 @@ include 'koneksi.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-
 // menyeleksi data user dengan username dan password yang sesuai
-$login = mysqli_query($koneksi,"select * from users where username='$username' and password='$password'");
+$login = mysqli_query($koneksi, "select * from users where username='" . $username . "' and password='" . $password . "'");
 // menghitung jumlah data yang ditemukan
 $cek = mysqli_num_rows($login);
 
 // cek apakah username dan password di temukan pada database
-if($cek > 0){
+if ($cek > 0) {
 
-	$data = mysqli_fetch_assoc($login);
+    $data = mysqli_fetch_assoc($login);
 
-	// cek jika user login sebagai petugas
-	if($data['status']=="aktif"){
+    // cek jika user login sebagai petugas
+    if ($data['status'] == "aktif") {
 
-		// buat session login dan username
-		$_SESSION['username'] = $username;
-		// alihkan ke halaman dashboard petugas
-		header("location:home.php");
+        // buat session login dan username
+        $_SESSION['username'] = $username;
+        // alihkan ke halaman dashboard petugas
+        header("location:home.php");
 
-	}else{
-		// alihkan ke halaman login kembali
-		header("location:index.php?pesan=gagal");
-	}
+    } else {
+        // alihkan ke halaman login kembali
+        header("location:index.php?pesan=gagal");
+    }
 
-	
-}else{
-	header("location:index.php?pesan=gagal");
+} else {
+    header("location:index.php?pesan=gagal");
 }
-?>
--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,7 @@
-<?php 
-$koneksi = mysqli_connect("localhost","root","","twm");
- 
+<?php
+$koneksi = mysqli_connect("localhost", "root", "root", "twm");
+
 // Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
+if (mysqli_connect_errno()) {
+    echo "Koneksi database gagal : " . mysqli_connect_error();
 }
- 
-?>
